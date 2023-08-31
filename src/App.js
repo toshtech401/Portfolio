83b9e1fc7918ea9b85439d42b467bb460ea57e40
od44 <akerelesamuel4@gmail.com>
2023-08-31 23:23:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Dictionary from './Components/Dictionary';
+
+function App() {
+  return (
+    <div className="App">
+      <Dictionary/>
+    </div>
+  );
+}
+
+export default App;
